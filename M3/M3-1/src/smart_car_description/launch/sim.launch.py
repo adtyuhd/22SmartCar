7927dd8e6e661e9a332a5bad54c7ae1ebe1be75f
adtyuhd <leilei_8@foxmail.com>
2026-09-24 01:48:55 -0400

    feat(m3-1): auto-start ros2 controllers

--- M3/M3-1/src/smart_car_description/launch/sim.launch.py
+++ M3/M3-1/src/smart_car_description/launch/sim.launch.py
@@ -69,9 +69,31 @@ def generate_launch_description():
         ],
         output='screen'
     )
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster',
+            '--controller-manager',
+            '/controller_manager'
+        ],
+        output='screen'
+    )
 
+    ackermann_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'ackermann_steering_controller',
+            '--controller-manager',
+            '/controller_manager'
+        ],
+        output='screen'
+    )
     return LaunchDescription([
         robot_state_publisher,
         gazebo,
-        spawn_car
+        spawn_car,
+        joint_state_broadcaster_spawner,
+        ackermann_controller_spawner
     ])
